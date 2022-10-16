ddf70b1ffd4c9292371fc9f099d7002afe58301b
tumanov-alex <uwontrememberthis@gmail.com>
2022-10-16 22:51:06 +0300

    why im not moving?

---
+++ App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, TouchableOpacity, Animated, Text} from 'react-native';
+
+import {useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
+
+const App = () => {
+  const offset = useSharedValue({x: 0, y: 0});
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: offset.value.x}, {translateY: offset.value.y}],
+  }));
+
+  return (
+    <View style={{flex: 1}}>
+      <TouchableOpacity
+        onPress={() => {
+          offset.value = {
+            x: Math.random() * 100,
+            y: Math.random() * 100,
+          };
+        }}>
+        <View style={{width: 500, height: 500, backgroundColor: 'grey'}} />
+      </TouchableOpacity>
+
+      <Animated.View
+        style={[
+          animatedStyle,
+          {
+            width: 100,
+            height: 100,
+            backgroundColor: 'blue',
+          },
+        ]}>
+        <Text style={{color: 'white'}}>Why I'm not moving?</Text>
+      </Animated.View>
+    </View>
+  );
+};
+
+export default App;
